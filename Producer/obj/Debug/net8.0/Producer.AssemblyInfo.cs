d4bb6f29cfa3ad5bd2d01d7a0b815f7509837c6e
belliorgabxl <64011224@kmitl.ac.th>
2024-05-28 10:08:29 +0700

    adding git read me

--- Producer/obj/Debug/net8.0/Producer.AssemblyInfo.cs
+++ Producer/obj/Debug/net8.0/Producer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Producer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10d9c7f40a3e0cd8a89c1a4af8af6927af3181f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Producer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Producer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ README..md
@@ -0,0 +1,1 @@
+# My English so bad
